6cc0de7ae4af0b505627b00aa4ba02a0f464c8c5
ihkaru <ihza2karunia@gmail.com>
2024-11-04 19:47:20 +0700

    fix animation

--- src/components/MainChart.vue
+++ src/components/MainChart.vue
@@ -64,6 +64,7 @@
 defineOptions({
   name: "MainChart",
 });
+import { easingEffects } from "chart.js/helpers";
 import { ref, computed, onMounted } from "vue";
 import { Line } from "vue-chartjs";
 import {
@@ -302,7 +303,48 @@ const handleChartLeave = () => {
     updateDisplayValues(currentData.price, initialData.price, currentData.date);
   }
 };
-
+let easing = easingEffects.easeInCirc;
+let restart = false;
+const totalDuration = 300;
+const duration = (ctx) =>
+  (easing(ctx.index / filteredData.value.length) * totalDuration) /
+  filteredData.value.length;
+const delay = (ctx) =>
+  easing(ctx.index / filteredData.value.length) * totalDuration;
+const previousY = (ctx) =>
+  ctx.index === 0
+    ? ctx.chart.scales.y.getPixelForValue(100)
+    : ctx.chart
+        .getDatasetMeta(ctx.datasetIndex)
+        .data[ctx.index - 1].getProps(["y"], true).y;
+const animation = {
+  x: {
+    type: "number",
+    easing: "linear",
+    duration: duration,
+    from: NaN, // the point is initially skipped
+    delay(ctx) {
+      if (ctx.type !== "data" || ctx.xStarted) {
+        return 0;
+      }
+      ctx.xStarted = true;
+      return delay(ctx);
+    },
+  },
+  y: {
+    type: "number",
+    easing: "linear",
+    duration: duration,
+    from: previousY,
+    delay(ctx) {
+      if (ctx.type !== "data" || ctx.yStarted) {
+        return 0;
+      }
+      ctx.yStarted = true;
+      return delay(ctx);
+    },
+  },
+};
 const chartOptions = {
   responsive: true,
   maintainAspectRatio: false,
@@ -314,6 +356,7 @@ const chartOptions = {
     legend: {
       display: false,
     },
+
     tooltip: {
       enabled: false,
     },
